d4a86c853a32efce22eb4d0cb2afa04f2ef7d513
longfish <loongfish@foxmail.com>
2021-10-19 22:22:23 -0700

    finished part of the linux_parser implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
+#include <iostream>
+
+#include "linux_parser.h"
 #include "ncurses_display.h"
 #include "system.h"
 
 int main() {
   System system;
-  NCursesDisplay::Display(system);
+  // NCursesDisplay::Display(system);
+
+  // test
+  std::cout << LinuxParser::MemoryUtilization() << "\n";
 }
