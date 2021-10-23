e737d0a70e3a81132f5aa7dfa3d458b1ed8c0d04
longfish <loongfish@foxmail.com>
2021-10-23 00:05:44 -0700

    rewrite the processor utilization function for accurate measuring

--- src/process.cpp
+++ src/process.cpp
@@ -3,10 +3,16 @@
 #include <unistd.h>
 
 #include <cctype>
+#include <iostream>
+#include <locale>
 #include <sstream>
 #include <string>
 #include <vector>
 
+#include "linux_parser.h"
+
+using std::cin;
+using std::locale;
 using std::string;
 using std::to_string;
 using std::vector;
@@ -23,8 +29,53 @@ string Process::Command() { return string(); }
 // TODO: Return this process's memory utilization
 string Process::Ram() { return string(); }
 
+// helper struct for seperate input stream using colon
+struct colon_is_space : std::ctype<char> {
+  colon_is_space() : std::ctype<char>(get_table()) {}
+  static mask const* get_table() {
+    static mask rc[table_size];
+    rc[':'] = std::ctype_base::space;
+    rc['\n'] = std::ctype_base::space;
+    return &rc[0];
+  }
+};
+
 // TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::User() {
+  // find the UID of the process
+  string key, line, uid, value;
+  std::ifstream filestream1(LinuxParser::kProcDirectory + to_string(id_) +
+                            LinuxParser::kStatusFilename);
+  if (filestream1.is_open()) {
+    while (std::getline(filestream1, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "Uid:") {
+          uid = value;
+        }
+      }
+    }
+  }
+
+  // find the corresponding username
+  // refer to Robᵩ answer:
+  // https://stackoverflow.com/questions/7302996/changing-the-delimiter-for-cin-c
+  string uname, x;
+  std::ifstream filestream2(LinuxParser::kPasswordPath);
+  if (filestream2.is_open()) {
+    while (std::getline(filestream2, line)) {
+      std::istringstream linestream(line);
+      linestream.imbue(locale(linestream.getloc(), new colon_is_space));
+
+      while (linestream >> key >> x >> value) {
+        if (value == uid) {
+          uname = key;
+        }
+      }
+    }
+  }
+  return uname;
+}
 
 // TODO: Return the age of this process (in seconds)
 long int Process::UpTime() { return 0; }
