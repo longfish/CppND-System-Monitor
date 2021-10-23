e9b30d6ac91193afed65e29755e8ca5e294cb8c8
longfish <loongfish@foxmail.com>
2021-10-23 00:04:27 -0700

    done the format function to convert second to hh:mm:ss

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,21 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds [[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  long hour = seconds / 3600;
+  long sec = seconds % 3600;
+
+  long min = sec / 60;
+  sec = sec % 60;
+
+  int nlimit = 2;  // limit length of the time number
+
+  string h = std::to_string(hour);
+  string m = std::to_string(min);
+  string s = std::to_string(sec);
+  h = string(nlimit - h.length(), '0') + h;
+  m = string(nlimit - m.length(), '0') + m;
+  s = string(nlimit - s.length(), '0') + s;
+
+  return h + ':' + m + ':' + s;
+}
