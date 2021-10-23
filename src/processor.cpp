9cc176b978dcfe25bd60516c5addc2e488bfbfb1
longfish <loongfish@foxmail.com>
2021-10-23 00:11:20 -0700

    rewrite the processor utilization function for accurate measuring

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,8 +1,15 @@
 #include "processor.h"
 
+#include <unistd.h>  // since this is a linux program
+
 #include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() {
-  return (float)LinuxParser::ActiveJiffies() / LinuxParser::Jiffies();
+  float prev_active = (float)LinuxParser::ActiveJiffies(),
+        prev_total = (float)LinuxParser::Jiffies();
+  sleep(1);
+  float cur_active = (float)LinuxParser::ActiveJiffies(),
+        cur_total = (float)LinuxParser::Jiffies();
+  return (cur_active - prev_active) / (cur_total - prev_total);
 }
