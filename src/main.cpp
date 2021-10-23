c5192302170c27e215c295722e6e0079b087b055
longfish <loongfish@foxmail.com>
2021-10-23 00:11:48 -0700

    tested the implemented functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <vector>
 
+#include "format.h"
 #include "linux_parser.h"
 #include "ncurses_display.h"
 #include "system.h"
 
 int main() {
   System system;
-  NCursesDisplay::Display(system);
+  // NCursesDisplay::Display(system);
 
   // test
-  // std::cout << LinuxParser::Jiffies() << "\n";
+  // std::cout << LinuxParser::UpTime() << "\n";
+  // std::cout << Format::ElapsedTime(system.UpTime()) << "\n";
   // std::vector<long> cpu_stat = LinuxParser::CpuUtilization();
   // for (auto ss : cpu_stat) {
   //   std::cout << ss << "\n";
   // }
+  Process proc;
+  proc.Pid(118);
+  std::cout << proc.User() << "\n";
 }
